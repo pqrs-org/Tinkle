68f18cde7504b3f2deb7aa806cfb1f77e4a2d885
Takayama Fumihiko <tekezo@pqrs.org>
2020-04-07 22:06:22 +0900

    add UserSettings.showMenu

--- src/Tinkle/swift/UserSettings.swift
+++ src/Tinkle/swift/UserSettings.swift
@@ -1,6 +1,22 @@
+import Combine
 import Foundation
 
 final class UserSettings: ObservableObject {
+    let objectWillChange = PassthroughSubject<Void, Never>()
+
     @UserDefault("effect", defaultValue: Effect.shockwaveBlue.rawValue)
     var effect: String
+
+    @UserDefault("showMenu", defaultValue: true)
+    var showMenu: Bool {
+        willSet {
+            objectWillChange.send()
+        }
+        didSet {
+            NotificationCenter.default.post(
+                name: Notification.Name("ShowMenuSettingChanged"),
+                object: nil
+            )
+        }
+    }
 }
