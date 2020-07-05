05b9f66febf3baf59c8cbbad68629c5a65e43702
Takayama Fumihiko <tekezo@pqrs.org>
2020-07-05 11:06:15 +0900

    Update indent

--- scripts/update-info-plist.sh
+++ scripts/update-info-plist.sh
@@ -8,10 +8,10 @@ version=$(head -n 1 version)
 sed "s|@TINKLE_VERSION@|$version|g" src/Tinkle/Info.plist.in >src/Tinkle/Info.plist.tmp
 
 if cmp -s src/Tinkle/Info.plist.tmp src/Tinkle/Info.plist; then
-  echo "src/Tinkle/Info.plist is skipped"
+    echo "src/Tinkle/Info.plist is skipped"
 else
-  cp src/Tinkle/Info.plist.tmp src/Tinkle/Info.plist
-  echo "src/Tinkle/Info.plist was updated"
+    cp src/Tinkle/Info.plist.tmp src/Tinkle/Info.plist
+    echo "src/Tinkle/Info.plist was updated"
 fi
 
 rm -f src/Tinkle/Info.plist.tmp
