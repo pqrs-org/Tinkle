080a3ad19248434272aab5c989f1520430a59ccd
Takayama Fumihiko <tekezo@pqrs.org>
2025-12-29 18:19:30 +0900

    Update AccessibilityAlertView.swift

--- src/Tinkle/swift/Views/AccessibilityAlertView.swift
+++ src/Tinkle/swift/Views/AccessibilityAlertView.swift
@@ -2,52 +2,45 @@ import SwiftUI
 
 struct AccessibilityAlertView: View {
   var body: some View {
-    HStack(alignment: .top, spacing: 20.0) {
-      VStack(alignment: .leading) {
-        Text("User approval for using accessibility features is required.")
-        Text("Tinkle uses the feature to detect the focused window changes.")
+    VStack(alignment: .center, spacing: 20.0) {
+      Label(
+        "User approval for using accessibility features is required.\n"
+          + "Tinkle uses the feature to detect the focused window changes.",
+        systemImage: "lightbulb"
+      )
 
-        Text("Open System Settings > Privacy & Security > Accessibility, then turn on Tinkle.")
-          .padding(
-            .top, 20.0)
-        Button(
-          action: {
-            NSWorkspace.shared.open(
-              URL(
-                string:
-                  "x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility")!)
-          },
-          label: {
-            Label(
-              "Open System Settings > Privacy & Security > Accessibility...",
-              systemImage: "arrow.forward.circle.fill")
-          })
+      Text("Open System Settings > Privacy & Security > Accessibility, then turn on Tinkle.")
 
-        Text("Restart Tinkle after you approve the feature.").padding(.top, 20.0)
-
-        Button(
-          action: { Relauncher.relaunch() },
-          label: {
-            Label("Restart Tinkle", systemImage: "arrow.clockwise")
-          })
-
-        Spacer()
-          .frame(height: 50.0)
-
-        Divider()
-
-        Button(
-          action: { NSApplication.shared.terminate(self) },
-          label: {
-            Label("Quit Tinkle", systemImage: "xmark.rectangle")
-          })
-      }.frame(minWidth: 400.0)
+      Button(
+        action: {
+          NSWorkspace.shared.open(
+            URL(
+              string:
+                "x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility")!)
+        },
+        label: {
+          Label(
+            "Open System Settings > Privacy & Security > Accessibility...",
+            systemImage: "arrow.forward.circle.fill")
+        })
 
       Image("accessibility")
         .resizable()
         .aspectRatio(contentMode: .fit)
         .frame(width: 400.0)
         .border(.gray, width: 1)
-    }.padding()
+
+      Divider()
+
+      Text("Please restart Tinkle after approving it.")
+
+      Button(
+        action: { Relauncher.relaunch() },
+        label: {
+          Label("Restart Tinkle", systemImage: "arrow.clockwise")
+        })
+    }
+    .padding()
+    .frame(width: 600)
   }
 }
